b8a09844de3e60079e053c88ddf08103a3354253
Do <mohosg011@gmail.com>
2023-03-09 11:06:27 +0700

    first commit

--- baitap/myapp/app.js
+++ baitap/myapp/app.js
@@ -1,6 +1,6 @@
 var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+var express = require('express1');
+var path = require('path1');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan1');
 
